7d062b2ba11c34f620b26d9c627ed85c536d92d4
Hyowon Bernabe <2244644@slu.edu.ph>
2024-08-22 00:30:38 +0800

    add object implements comparable add compareTo method template add sorting method templates

--- src/SortingAlgorithmBenchmark.java
+++ src/SortingAlgorithmBenchmark.java
@@ -11,7 +11,16 @@
  * @see SortingAlgorithmCounter
  */
 public class SortingAlgorithmBenchmark {
-    // should have an object of SortingAlgorithmCounter
-    // this is where to read csv/txt files and place it in an array type of <Object> (to be renamed).
-    // Main Class, read files, call methods, and output results (if necessary)
+    public static void main(String[] args) {
+        // should have an object of SortingAlgorithmCounter
+        // this is where to read csv/txt files and place it in an array type of <Object> (to be renamed).
+        // Main Class, read files, call methods, and output results (if necessary)
+    }
+
+    private static void selectionSort(Object[] objectToBeRenamed) {
+    }
+    private static void insertionSort(Object[] objectToBeRenamed) {
+    }
+    private static void bubbleSort(Object[] objectToBeRenamed) {
+    }
 }
